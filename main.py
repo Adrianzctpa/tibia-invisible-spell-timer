24f1da9ead5321c36305f734b60ef5371f48961a
Adrianzctpa <nocommentaryadrian@gmail.com>
2023-08-05 20:56:29 -0300

    visible timer

--- main.py
+++ main.py
@@ -7,12 +7,19 @@ key = '<shift>+r'
 exitKey = '<ctrl>+e'
 seconds = 180
 
+def countdown(seconds): 
+    while seconds:
+        mins, secs = divmod(seconds, 60)
+        timeformat = '{:02d}:{:02d}'.format(mins, secs)
+        print(timeformat, end='\r')
+        time.sleep(1)
+        seconds -= 1
+
 def main():
-    print('Press ' + key + ' to run timer again and ' + exitKey + ' to exit.')
+    print('Press ' + key + ' to run timer and ' + exitKey + ' to exit.')
 
     def on_activate():
-        print('Running...')
-        time.sleep(seconds)
+        countdown(seconds)
         call(['aplay', 'sound.wav'])
         main()
     

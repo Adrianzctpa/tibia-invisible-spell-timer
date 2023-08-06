9ab259359b5bfbbbe603046768acef52900499a4
Adrianzctpa <nocommentaryadrian@gmail.com>
2023-08-06 19:03:29 -0300

    beep mode

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from pynput import keyboard as kb
 import time
 from subprocess import call
+import os
 
 # You may change your config here
 key = '<shift>+r'
 exitKey = '<ctrl>+e'
 seconds = 180
+beep = True
 
 def countdown(seconds): 
     while seconds:
@@ -15,12 +17,19 @@ def countdown(seconds):
         time.sleep(1)
         seconds -= 1
 
+def play():
+    if beep:
+        call(['beep', '-f', '5000', '-l', '50', '-r', '2'])
+    else:
+        call(['aplay', 'sound.wav'])
+
 def main():
+    os.system('clear')
     print('Press ' + key + ' to run timer and ' + exitKey + ' to exit.')
 
     def on_activate():
         countdown(seconds)
-        call(['aplay', 'sound.wav'])
+        play()
         main()
     
     def on_activate_close():
